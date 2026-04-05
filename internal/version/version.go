430e37435b95d9f298e3f1c16b83995c016f6be6
anonvector <258689915+anonvector@users.noreply.github.com>
2026-04-05 14:35:32 +0200

    Bump version to 1.6.0-dev

--- internal/version/version.go
+++ internal/version/version.go
@@ -3,7 +3,7 @@ package version
 import "fmt"
 
 var (
-	Version    = "1.4.0"
+	Version    = "1.6.0-dev"
 	Commit     = "unknown"
 	ReleaseTag = "" // set via ldflags for dev builds (e.g. "dev-abc1234")
 )
